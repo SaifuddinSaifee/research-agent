ecf1b521ac83d0551cd7170a550fa2bda62aeaca
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2023-11-19 23:32:53 +0530

    Tool added, Base prompt added

--- app.py
+++ app.py
@@ -28,14 +28,9 @@ serper_api_key = os.getenv("SERP_API_KEY")
 def search(query):
     url = "https://google.serper.dev/search"
 
-    payload = json.dumps({
-        "q": query
-    })
+    payload = json.dumps({"q": query})
 
-    headers = {
-        'X-API-KEY': serper_api_key,
-        'Content-Type': 'application/json'
-    }
+    headers = {"X-API-KEY": serper_api_key, "Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
@@ -43,7 +38,9 @@ def search(query):
 
     return response.text
 
+
 # search("What is meta's tread product?") # test
+
 
 # 2. Tool for scraping
 def scrape_website(objective: str, url: str):
@@ -53,14 +50,12 @@ def scrape_website(objective: str, url: str):
     print("Scraping website...")
     # Define the headers for the request
     headers = {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
     }
 
     # Define the data to be sent in the request
-    data = {
-        "url": url
-    }
+    data = {"url": url}
 
     # Convert Python object to JSON string
     data_json = json.dumps(data)
@@ -70,7 +65,7 @@ def scrape_website(objective: str, url: str):
     response = requests.post(post_url, headers=headers, data=data_json)
 
     # Check the response status code
-    if response.status_code == 200: # Successfully extreacted content
+    if response.status_code == 200:  # Successfully extreacted content
         soup = BeautifulSoup(response.content, "html.parser")
         text = soup.get_text()
         print("CONTENT:", text)
@@ -83,13 +78,16 @@ def scrape_website(objective: str, url: str):
     else:
         print(f"HTTP request failed with status code {response.status_code}")
 
+
 # scrape_website("What is langChain", "https://www.langchain.com/") # Test
+
 
 def summary(objective, content):
     llm = ChatOpenAI(temperature=0, model="gpt-4-1106-preview")
 
     text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
+        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500
+    )
     docs = text_splitter.create_documents([content])
     map_prompt = """
     Write a summary of the following text for {objective}:
@@ -97,29 +95,36 @@ def summary(objective, content):
     SUMMARY:
     """
     map_prompt_template = PromptTemplate(
-        template=map_prompt, input_variables=["text", "objective"])
+        template=map_prompt, input_variables=["text", "objective"]
+    )
 
     summary_chain = load_summarize_chain(
         llm=llm,
-        chain_type='map_reduce',
+        chain_type="map_reduce",
         map_prompt=map_prompt_template,
         combine_prompt=map_prompt_template,
-        verbose=True
+        verbose=True,
     )
 
     output = summary_chain.run(input_documents=docs, objective=objective)
 
     return output
 
+
 # Handling Multiple inputs from the scrapped website
+
 
 class ScrapeWebsiteInput(BaseModel):
     """Inputs for scrape_website"""
+
     objective: str = Field(
-        description="The objective & task that users give to the agent")
+        description="The objective & task that users give to the agent"
+    )
     url: str = Field(description="The url of the website to be scraped")
 
+
 # Handling Multiple url from the scrapped website
+
 
 class ScrapeWebsiteTool(BaseTool):
     name = "scrape_website"
@@ -132,3 +137,29 @@ class ScrapeWebsiteTool(BaseTool):
     def _arun(self, url: str):
         raise NotImplementedError("error here")
 
+
+# 3. Creating the agent
+
+tools = [
+    Tool(
+        name="Search",
+        func=search,
+        description="To answer questions on data and current events. Ask targeted questions",
+    ),
+    ScrapeWebsiteTool(),
+]
+
+# # Prompt
+
+system_message = SystemMessage(
+    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            
+            Please make sure you complete the objective above with the following rules:
+            1/ You should do enough research to gather as much information as possible about the objective
+            2/ If there are url of relevant links & articles, you will scrape it to gather more information
+            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+            4/ You should not make things up, you should only write facts & data that you have gathered
+            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+)
